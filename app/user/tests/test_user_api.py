60611d39271d1d290344d067bf7c787de9256631
Guillermo Celarie <lg.celarie@gmail.com>
2022-09-18 22:23:48 -0600

    fixes on create user tests. Implementation of functionality on API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -45,10 +45,10 @@ class PublicUserAPITest(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        create_user(payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
         """Test an error is resturned if password less than 5 chars"""
